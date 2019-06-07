86dbf58d701604fc2eb3017013ab6c099dd47c8d
Kate  Reeher <kate.reeher@revel-health.com>
2019-06-07 14:16:42 -0500

    added need for auth on endpoint

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,12 +1,22 @@
+# snippets/serializers.py
 
-# snippets/serializers
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Snippet
         fields = ('id', 'title', 'code', 'linenos',
-                  'language', 'style', )
+                  'language', 'style', 'owner',)
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
--- snippets/views.py
+++ snippets/views.py
@@ -1,13 +1,28 @@
 # snippets/views.py
-from django.shortcuts import render
-from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
+
 from .models import Snippet 
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
  
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    
+class UserList(generics.ListAPIView): # new
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView): # new
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
